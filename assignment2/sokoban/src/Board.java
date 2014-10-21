0720b3805e7f8f6c2427030a3c9af16794cfc1ae
Thom Ritterfeld <thom@ritterfeld.com>
2014-10-21 10:37:31 +0200

    sokoban

--- assignment2/sokoban/src/Board.java
+++ assignment2/sokoban/src/Board.java
@@ -7,12 +7,29 @@ final class Board {
   /** @informal All marked locations are in the valid playing area (that is, on the ground) */
   /** @informal All crates are in the valid playing area */
 	
-  int xSize; 
-  int ySize; 
-
-  public BoardItem[][] items;
-
+	/*@ spec_public @*/int xSize; 
+	//@ public invariant xSize > 0 && xSize == ySize;
+	  
+		/*@ spec_public @*/int ySize; 
+	//@ public invariant ySize > 0 && xSize == ySize;
+	  
+	  public BoardItem[][] items;
+		//@ public invariant items.length == xSize &&  items.length == ySize;
+	  /*@ public invariant 
+	  @  (\forall int i; 0 <= i && i < items.length ==> 
+	  @    (items[i].length == items.length));
+	  @*/
+  
   /** @informal based on valid parameters the constructor creates an "all wall" board */
+	  /*@ normal_behavior
+      @   requires  xSize > 0 && ySize > 0 ;
+      @   ensures  true;
+      @ also
+      @ exceptional_behavior
+      @   requires  xSize <= 0 || ySize <= 0;
+      @   signals_only NegativeArraySizeException;
+      @   signals  (NegativeArraySizeException) true;
+      @*/
   Board (int xSize, int ySize) {
     this.xSize = xSize;
     this.ySize = ySize;
@@ -25,18 +42,24 @@ final class Board {
   }
   
   /** @informal auxiliary method to establish that a position is on the board */
+  /*@ public normal_behavior
+  @   ensures  \result == (0 <= p.x && p.x < xSize && 0 <= p.y && p.y < ySize);
+  @*/
   public boolean onBoard(Position p) {
       return 0 <= p.x && p.x < xSize && 0 <= p.y && p.y < ySize;
   }
 
   /** @informal same as above for explicit coordinates */
+  /*@ public normal_behavior
+  @   ensures  \result == (0 <= x && x < xSize && 0 <= y && y < ySize);
+  @*/
   public boolean onBoard(int x, int y) {
       return 0 <= x && x < xSize && 0 <= y && y < ySize;
   }
 
   /** @informal auxiliary method to establish that a position is on board and is open
    *     (the player can stand on it) */
-  public boolean isOpen(/*@ non_null @*/ Position p) {
+  public boolean isOpen(/*@ non_null @*/Position p) {
 	  if(!onBoard(p)) return false;
 	  return items[p.x][p.y].ground && !items[p.x][p.y].crate; 
   }
@@ -44,7 +67,7 @@ final class Board {
   /** @informal same as above for explicit coordinates */
   public boolean isOpen(int x, int y) {
 	  if(!onBoard(x, y)) return false;
-	  return items[x][y].ground && !items[x][y].crate; 
+	  return items[x][y].ground && !items[x][y].crate;   
   }
 
   //@ skipesc
--- assignment2/sokoban/src/Position.java
+++ assignment2/sokoban/src/Position.java
@@ -4,17 +4,28 @@
 final class Position
 {
 
-  /** @informal only allow positive positions */
-  final int x;
-  final int y;
-
-  /** @informal based on valid parameters the constructor creates a valid position object */
+  /** @informal only allow positive positions 
+	*/
+  /*@ public invariant this.x > 0 && this.y > 0;
+  
+  /*@ spec_public */ final int x;
+  
+  /*@ spec_public */ final int y;
+  
+  /** @informal based on valid parameters the constructor creates a valid position object */ 
+  /*@ 
+  modifies this.x,this.y;
+  requires true;
+*/
   Position (int x, int y) {
     this.x = x;
     this.y = y;
   }
 
-  /** @informal to be equal positions need to agree on both coordinates */ 
+  /** @informal to be equal positions need to agree on both coordinates  */
+  /*  normal_behavior
+      @requires o != null
+   	  @ensures y == q; */
   public boolean equals (Object o) {
     if (o instanceof Position) {
       Position q = (Position) o;
@@ -25,9 +36,14 @@ final class Position
 
   /** @informal a valid next position is away one move horizontally
    *    or vertically from the current one */
+  /*  normal_behavior
+   	  @requires o != null
+	  @ensures y == q; */
   boolean isValidNextPosition (Position newPosition) {
 	  int dX = newPosition.x - x;
+	  // @assert dx == newPosition.x - x;
 	  int dY = newPosition.y - y;
+	// @assert dx == newPosition.x - x;
 	  if( dX >= -1 && dX <= 1 && dY >= -1 && dY <= 1) return true;
 	  return false;
 	  }
